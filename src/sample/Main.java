a659c3f54386647d6369f3417bd5eff544999d64
nowrinahmed <84222490+nowrinahmed@users.noreply.github.com>
2021-05-15 22:35:14 +0600

    good

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LogInForm.fxml"));
-        primaryStage.setTitle("anything ");
+        primaryStage.setTitle("anything good");
         primaryStage.setScene(new Scene(root));
         primaryStage.sizeToScene();
         primaryStage.show();
