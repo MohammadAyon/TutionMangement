f0277c9c87ecdf2771c1cdd8916da60678764757
mohammad-Ayon <mohammadayon6969@gmail.com>
2021-05-16 13:58:07 +0600

    rearranging Package

--- src/AllControlers/LoginFormController.java
+++ src/AllControlers/LoginFormController.java
@@ -4,15 +4,12 @@ package AllControlers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class LoginFormController {
 
@@ -30,13 +27,14 @@ public class LoginFormController {
     System.out.println("logged IN");
     System.out.println(this.UserNameText.getText()+"\n"+this.passwordholder.getText());
     FXMLLoader loader =new FXMLLoader(getClass().getResource("AllFxmlFile/AdminDashBoard.fxml"));
-    AdminDashBoardController c =loader.getController();
-    Parent root = loader.load();
-    Stage stage=new Stage();
-    stage.setTitle("anything good");
-    stage.setScene(new Scene(root));
-    stage.sizeToScene();
-    stage.show();
+    AdminDashBoardController Admin =loader.getController();
+    Pane root = (Pane)loader.load();
+    Stage AdminStage=new Stage();
+    Scene AdminScene=new Scene(root);
+    AdminStage.setTitle("anything good");
+    AdminStage.setScene(AdminScene);
+    AdminStage.sizeToScene();
+    AdminStage.show();
   
   }
   @FXML
