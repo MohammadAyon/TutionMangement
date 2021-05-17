067be2f1b1c62a74564fb8c7d5c5ed6e25b38cf1
nowrinahmed <84222490+nowrinahmed@users.noreply.github.com>
2021-05-18 01:03:13 +0600

    updated person class information

--- src/Functionalities/person.java
+++ src/Functionalities/person.java
@@ -1,8 +1,76 @@
 package Functionalities;
 
 public class person {
- private  String Firsname;
- private String LastName;
- private String InistuitionName;
- private String EmailAddress;
+ private  String firstName;
+ private String lastName;
+ private String nationalId;
+ private String gender;
+ private String phoneNumber;
+ private int dateOfBirth;
+ private String institutionName;
+ private String emailAddress;
+
+ 
+ public person(String firstName,String lastName,String nationalId,String gender,String phoneNumber,
+               int dateOfBirth,String institutionName,String emailAddress){
+  this.firstName=firstName;
+  this.lastName=lastName;
+  this.nationalId=nationalId;
+  this.gender=gender;
+  this.phoneNumber=phoneNumber;
+  this.dateOfBirth=dateOfBirth;
+  this.institutionName=institutionName;
+  this.emailAddress=emailAddress;
+ }
+ public String getFirstName(){
+  return firstName;
+ 
+ }
+ public void setFirstName(String firstName){
+  this.firstName=firstName;
+ }
+ public String getLastName(){
+  return lastName;
+ }
+ public void setLastName(String lastName){
+  this.lastName=lastName;
+ }
+ public String getNationalId(){
+  return nationalId;
+ }
+ public void setNationalId(String nationalId){
+  this.nationalId=nationalId;
+ }
+public String getGender(){
+  return gender;
 }
+public void setGender(String gender){
+  this.gender=gender;
+}
+public String getPhoneNumber(){
+  return phoneNumber;
+}
+public void setPhoneNumber(String phoneNumber){
+  this.phoneNumber=phoneNumber;
+}
+public int getDateOfBirth(){
+  return dateOfBirth;
+}
+public void setDateOfBirth(int dateOfBirth){
+  this.dateOfBirth=dateOfBirth;
+}
+public String getInstitutionName(){
+  return  institutionName;
+}
+public void setInstitutionName(String institutionName){
+  this.institutionName=institutionName;
+}
+public String getEmailAddress(){
+  return  emailAddress;
+}
+public void setEmailAddress(String emailAddress){
+  this.emailAddress=emailAddress;
+}
+
+}
+
