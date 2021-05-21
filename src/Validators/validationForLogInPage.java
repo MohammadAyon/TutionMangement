91f4f77a71b6b813a5ddfd71145e3246ba7cdce0
Iazdanul-Haque <84215352+Iazdanul-Haque@users.noreply.github.com>
2021-05-21 16:54:40 +0600

    validationForLogInPage
    Added commands to check for null validations during login page.

--- src/Validators/validationForLogInPage.java
+++ src/Validators/validationForLogInPage.java
@@ -1,4 +1,18 @@
 package Validators;
 
 public class validationForLogInPage {
+  public static boolean UserName(String userIdName) throws Exception {
+    if (userIdName != null) {
+      return true;
+    }
+    throw new Exception("You did not sign in the user Name correctly!");
+  }
+
+  public static boolean passwordHolder(String userPassword) throws Exception {
+    if (userPassword != null) {
+      return true;
+    }
+
+    throw new Exception("You did not sign in the password correctly!");
+  }
 }
